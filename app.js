4240ba01eb412d5fcc5bb8d9c63cf4fbf6eab631
periman22 <periman@windowslive.com>
2017-02-09 16:22:17 +0200

    added env variables

--- app.js
+++ app.js
@@ -53,9 +53,8 @@ app.use(friendRoutes);
 app.use(groupRoutes);
 app.use(arrayRoutes);
 app.use(indexRoutes);
-mongodb = 
-//mongoose.connect("mongodb://localhost/coordinator25");
-mongoose.connect("mongodb://myname:wkVRAhWELq@ds145639.mlab.com:45639/timedup");
+
+mongoose.connect(process.env.DATABASEURL);
 var PORT = process.env.PORT || 3000;
 
 
