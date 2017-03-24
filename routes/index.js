b63e7a20ea50de4c8d450027de23ff0dc7827199
periman22 <periman@windowslive.com>
2017-03-24 22:15:18 +0200

    testing penguins

--- routes/index.js
+++ routes/index.js
@@ -21,25 +21,24 @@ router.get("/slack/botauth", function(req, res){
         client_secret: process.env.SLACK_CLIENT_SECRET_OFBOT,
         code: req.query.code
     }};
-    var token = JSON.parse(body).access_token;
+    let token = JSON.parse(body).access_token;
     request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     let teamid = JSON.parse(body).team.id;
                     let teamname = JSON.parse(body).team.id;
-                    Team.find({name: teamname, id: teamid}).exec()
-                    .then(function(foundteam){
+                    Team.find({name: teamname, id: teamid}, function(err, foundteam){
                         if(foundteam.length > 0 && foundtam){
                             return send("Another person from the team already added the app.");
                         }
                         Team.create({name: teamname, id: teamid, token: token}, function(err, newteam){
                             console.log("this is a new team " + newteam);
-                            res.redirect("/")
+                            res.redirect("/");
                         });
-                    }).catch(function(err){
-                        throw err;
-                    });
+                    })
+                } else {
+                    return send("error happend with ")
                 }
             });
         }
