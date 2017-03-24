9e1212c8ff6b58e7e0e9746f3ae90acb3d4acd96
periman22 <periman@windowslive.com>
2017-03-24 18:20:18 +0200

    more testing

--- routes/index.js
+++ routes/index.js
@@ -164,7 +164,7 @@ router.post("/timedup-makegroup", function(req, res){
                     console.log("this is the body", req.body);
                     if(slackuser[0]){
                         request.post('https://slack.com/api/groups.info', {form: {token: process.env.SLACK_OAUTH_TOKEN, channel: req.body.channel_id}}, function (error, response, body) {
-                            console.log(response.statusCode, JSON.parse(body));
+                            console.log(response.statusCode, JSON.parse(body) + "this is inside the groups now" + process.env.SLACK_OAUTH_TOKEN);
                             if (!error && response.statusCode == 200 && JSON.parse(body).ok) {
                                 console.log("haha " + JSON.parse(body));
                                 let channelmembers = JSON.parse(body).group.members;
